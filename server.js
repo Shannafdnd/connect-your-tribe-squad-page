5f922a57c268a3c19af43c7be445b9c1c8962102
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2024-02-27 11:36:19 +0100

    messages added

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// 1. SERVER OPZETTEN
+// *** Express setup en start ***
 
 // Importeer het npm pakket express uit de node_modules map
 import express from 'express'
@@ -8,9 +8,6 @@ import fetchJson from './helpers/fetch-json.js'
 
 // Stel het basis endpoint in
 const apiUrl = 'https://fdnd.directus.app/items'
-
-// Haal alle squads uit de WHOIS API op
-const squadData = await fetchJson(apiUrl + '/squad')
 
 // Maak een nieuwe express app aan
 const app = express()
@@ -24,26 +21,50 @@ app.set('views', './views')
 // Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
 app.use(express.static('public'))
 
-// 2. HTTP REQUEST RESPONSES
+app.use(express.urlencoded({extended: true}))
 
-// Stap1
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set('port', process.env.PORT || 8000)
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+
+//*** Data ***
+
+// Haal alle squads uit de WHOIS API op
+const squadData = await fetchJson(apiUrl + '/squad')
+
+// Zet een array klaar, waarin berichten worden opgeslagen.
+const messages = []
+
+
+
+// *** Routes ***
+
+
 // Maak een GET route voor de index
 app.get('/', function (request, response) {
   // Haal alle personen uit de WHOIS API op
 
-  // Stap 2
-  fetchJson('https://fdnd.directus.app/items/person').then((apiData) => {
+  fetchJson( apiUrl + '/person').then((apiData) => {
     // apiData bevat gegevens van alle personen uit alle squads
     // Je zou dat hier kunnen filteren, sorteren, of zelfs aanpassen, voordat je het doorgeeft aan de view
 
-    // Stap 3
     // Render index.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
-    response.render('index', {persons: apiData.data, squads: squadData.data})
+    response.render('index', 
+    {persons: apiData.data,
+    squads: squadData.data,
+    messages: messages})
   })
 })
 
 // Maak een POST route voor de index
 app.post('/', function (request, response) {
+  messages.push(request.body.bericht)
   // Er is nog geen afhandeling van POST, redirect naar GET op /
   response.redirect(303, '/')
 })
@@ -63,13 +84,4 @@ app.get('/squad/:squad_id', function (request, response) {
     // Render person.ejs uit de views map en geef de opgehaalde data mee als variable, genaamd person
     response.render('person', {persons: persons.data, squads: squadData.data})
   })
-})
-
-// Stel het poortnummer in waar express op moet gaan luisteren
-app.set('port', process.env.PORT || 8000)
-
-// Start express op, haal daarbij het zojuist ingestelde poortnummer op
-app.listen(app.get('port'), function () {
-  // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
 })
